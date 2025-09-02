59e66028cc047a2cc32d85d72aa1952d18e69815
Takauã Silva <takauan9@gmail.com>
2025-09-02 00:06:23 +0000

    Super Trunfo - Nivel Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,80 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
+    char codigo1[10], codigo2[10], cidade1[50], cidade2[50], estado1[10], estado2[10];
+    int populacao1, populacao2, pontos_turisticos1, pontos_turisticos2;
+    float area1, area2, pib1, pib2;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // Seção de captura de dados da carta 1
+    printf("\nCapturando informações da Carta 1\n\n");
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("- Estado: ");
+    scanf("%s", estado1); // Captura o estado 1
+
+    printf("- Código: ");
+    scanf("%s", codigo1); // Captura o código 1
+
+    printf("- Nome da Cidade: ");
+    scanf("%s", cidade1); // Captura o nome de cidade 1
+
+    printf("- População: ");
+    scanf("%d", &populacao1); // Captura população 1
+
+    printf("- Área: ");
+    scanf("%f", &area1); // Captura a área 1
+
+    printf("- PIB: ");
+    scanf("%f", &pib1); // Captura o PIB 1
+
+    printf("- Número de Pontos Turísticos: ");
+    scanf("%d", &pontos_turisticos1); // Captura o números de pontos
+
+    // Seção de captura de dados da carta 2
+    printf("\nCapturando informações da Carta 2\n\n");
+    getchar(); // Limpar o buffer
+
+    printf("- Estado: ");
+    scanf("%s", estado2); // Captura o estado 2
+
+    printf("- Código: ");
+    scanf("%s", codigo2); // Captura o código 2
+
+    printf("- Nome da Cidade: ");
+    scanf("%s", cidade2); // Captura o nome de cidade 2
+
+    printf("- População: ");
+    scanf("%d", &populacao2); // Captura a população 2
+
+    printf("- Área: ");
+    scanf("%f", &area2); // Captura a área 2
+
+    printf("- PIB: ");
+    scanf("%f", &pib2); // Captura o PIB 2
+
+    printf("- Número de Pontos Turísticos: ");
+    scanf("%d", &pontos_turisticos2); // Captura o número de pontos 2
+
+    // Seção responsável por imprimir as informações da carta 1
+    printf("\nCarta 1:\n");
+
+    printf("• Estado: %s\n", estado1);
+    printf("• Código: %s\n", codigo1);
+    printf("• Nome da Cidade: %s\n", cidade1);
+    printf("• População: %d\n", populacao1);
+    printf("• Área: %.2f km²\n", area1);
+    printf("• PIB: %.2f\n", pib1);
+    printf("• Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+
+    // Seção responsável por imprimir as informações da carta 2
+    printf("\nCarta 2:\n");
+
+    printf("• Estado: %s\n", estado2);
+    printf("• Código: %s\n", codigo2);
+    printf("• Nome da Cidade: %s\n", cidade2);
+    printf("• População: %d\n", populacao2);
+    printf("• Área: %.2f km²\n", area2);
+    printf("• PIB: %.2f\n", pib2);
+    printf("• Número de Pontos Turísticos: %d", pontos_turisticos2);
 
     return 0;
 }
